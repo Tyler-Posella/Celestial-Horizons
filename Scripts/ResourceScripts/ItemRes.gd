43ec9176e53d13e0023f16d190b5950a7dc9d213
Tyler Posella <tyler.joseph.posella@gmail.com>
2024-10-14 19:26:07 -0700

    Added Fruit and Berries
    Added all fruit and berry resources

--- Scripts/ResourceScripts/ItemRes.gd
+++ Scripts/ResourceScripts/ItemRes.gd
@@ -5,7 +5,6 @@ extends Resource
 @export var name : String = "None"
 @export var texture : AtlasTexture = null
 @export var description : String = "None"
-@export var sell_price : int
 
 # Functions
 func get_item_name():
@@ -18,7 +17,3 @@ func get_texture():
 
 func get_description():
 	return description
-	
-
-func get_sell_price():
-	return sell_price
